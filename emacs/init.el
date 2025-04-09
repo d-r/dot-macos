4f6ee514b1b5302906b9e51cb808414d83db6961
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2025-04-09 20:30:56 +0200

    emacs: change Denote dir and keywords

--- emacs/init.el
+++ emacs/init.el
@@ -206,8 +206,8 @@
 
 (use-package denote
   :init
-  (setq denote-directory (expand-file-name "~/org/denote"))
-  (setq denote-known-keywords '("howto" "project" "person" "draft"))
+  (setq denote-directory (expand-file-name "~/notes"))
+  (setq denote-known-keywords '("je" "pr" "pe" "no"))
   (setq denote-backlinks-show-context t)
   (setq denote-sort-keywords nil)
 
