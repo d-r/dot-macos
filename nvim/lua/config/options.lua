426e104d8f2426cf7bdc2fb6f831d412db3da986
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2025-05-01 15:20:10 +0200

    nvim: don't show the current mode

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -17,6 +17,9 @@ vim.opt.relativenumber = true
 -- Enable mouse mode
 vim.opt.mouse = 'a'
 
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
 -- Sync clipboard between OS and Neovim
 -- Schedule the setting after `UiEnter` because it can increase startup-time
 -- See `:help 'clipboard'`
