1fbcd60e954e39d6cc69c1610b91c4a3e9997424
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2025-05-02 09:33:10 +0200

    nvim: install `neogit`

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,9 @@
+-- https://github.com/NeogitOrg/neogit
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+}
